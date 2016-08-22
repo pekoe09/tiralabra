1b1d11dc12562bbb2d694740d153d6a2ffcecbbd
Juha <juha.kangas@welho.com>
2016-08-23 00:08:37 +0300

    Lisätty pari testiä

--- src/test/java/tiralabra/search/PathSearcherTest.java
+++ src/test/java/tiralabra/search/PathSearcherTest.java
@@ -1,46 +1,109 @@
 package tiralabra.search;
 
-import java.util.List;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
 import tiralabra.datainput.IDataMapper;
+import tiralabra.datainput.PlaceGraphMapper;
+import tiralabra.datastructures.NamedArrayList;
+import tiralabra.domain.NeighbourNode;
 import tiralabra.domain.PathSearchResult;
 import tiralabra.domain.PlaceNode;
 
 public class PathSearcherTest {
     
     private PathSearcher testSearcher;
-    private List<PlaceNode> graph;
+    private IDataMapper mapper;
+    private NamedArrayList graph;
     private String startPlaceName;
     private String endPlaceName;
     private String filePath;
     private long edgeCount;
     private int nodeCount;
+    PlaceNode helsinki, espoo, vantaa, kauniainen, sipoo;
 
     @Before
     public void setUp() {
+        testSearcher = new PathSearcher();
+        
+        helsinki = new PlaceNode("Helsinki", 60.171160, 24.932580);
+        espoo = new PlaceNode("Espoo", 60.206780, 24.655780);
+        vantaa = new PlaceNode("Vantaa", 60.289348, 25.029676);
+        kauniainen = new PlaceNode("Kauniainen", 60.211642, 24.729048);
+        sipoo = new PlaceNode("Sipoo", 60.376721, 25.260191);
+        helsinki.setNeighbours(new NeighbourNode[] {
+                                new NeighbourNode(espoo, 15.805679),
+                                new NeighbourNode(vantaa, 14.193181),
+                                new NeighbourNode(sipoo, 29.133081)});
+        espoo.setNeighbours(new NeighbourNode[] {
+                                new NeighbourNode(helsinki, 15.805679),
+                                new NeighbourNode((vantaa), 22.582160),
+                                new NeighbourNode(kauniainen, 4.083670)});
+        vantaa.setNeighbours(new NeighbourNode[]{
+                                new NeighbourNode(espoo, 22.582160),
+                                new NeighbourNode(helsinki, 14.193181),
+                                new NeighbourNode(sipoo, 15.979494)});
+        kauniainen.setNeighbours(new NeighbourNode[]{
+                                new NeighbourNode(espoo, 4.083670)});
+        sipoo.setNeighbours(new NeighbourNode[]{
+                                new NeighbourNode(helsinki, 29.133081),
+                                new NeighbourNode(vantaa, 15.979494)});
+        
+        graph = new NamedArrayList();
+        graph.add(helsinki);
+        graph.add(espoo);
+        graph.add(vantaa);
+        graph.add(kauniainen);
+        graph.add(sipoo);
+        
+        startPlaceName = helsinki.getName();
+        endPlaceName = espoo.getName();
+        edgeCount = 7;
+        nodeCount = 5;
+        filePath = "joku/polku";
+        
+        mapper = Mockito.mock(PlaceGraphMapper.class);
+        when(mapper.getData()).thenReturn(graph);
+        when(mapper.getNumberOfEdges()).thenReturn(edgeCount);
     }
 
     @Test
     public void testRunAlgosWithGraph() {
-//        PathSearchResult[] testResults = testSearcher.runAlgos(
-//                graph, 
-//                startPlaceName, 
-//                endPlaceName, 
-//                filePath, 
-//                edgeCount, 
-//                nodeCount);
-//        assertNotNull("Algoritmiajo ei palauta lainkaan tuloksia", testResults);
-//        assertEquals("Algoritmiajo ei tuottanut täsmälleen kahta tulosta", 2, testResults.length);
+        PathSearchResult[] testResults = testSearcher.runAlgos(
+                graph, 
+                startPlaceName, 
+                endPlaceName, 
+                filePath, 
+                edgeCount, 
+                nodeCount);
+        assertNotNull("Algoritmiajo ei palauta lainkaan tuloksia", testResults);
+        assertEquals("Algoritmiajo ei tuottanut täsmälleen kahta tulosta", 2, testResults.length);
+        assertEquals("Ensimmäisen tuloksen kaariluku on väärin", (long)edgeCount, (long)testResults[0].getEdgeCount());
+        assertEquals("Toisen tuloksen kaariluku on väärin", (long)edgeCount, (long)testResults[1].getEdgeCount());
+        assertEquals("Ensimmäisen tuloksen solmuluku on väärin", nodeCount, testResults[0].getNodeCount());
+        assertEquals("Toisen tuloksen solmuluku on väärin", nodeCount, testResults[1].getNodeCount());
+        assertEquals("Ensimmäisen tuloksen tiedostopolku on väärin", filePath, testResults[0].getFilePath());
+        assertEquals("Toisen tuloksen tiedostopolku on väärin", filePath, testResults[1].getFilePath());
     }
     
     @Test
     public void testRunAlgosWithMapper() {
-
+    PathSearchResult[] testResults = testSearcher.runAlgos(
+                mapper, 
+                startPlaceName, 
+                endPlaceName, 
+                filePath);
+        assertNotNull("Algoritmiajo ei palauta lainkaan tuloksia", testResults);
+        assertEquals("Algoritmiajo ei tuottanut täsmälleen kahta tulosta", 2, testResults.length);
+        assertEquals("Ensimmäisen tuloksen kaariluku on väärin", (long)edgeCount, (long)testResults[0].getEdgeCount());
+        assertEquals("Toisen tuloksen kaariluku on väärin", (long)edgeCount, (long)testResults[1].getEdgeCount());
+        assertEquals("Ensimmäisen tuloksen solmuluku on väärin", nodeCount, testResults[0].getNodeCount());
+        assertEquals("Toisen tuloksen solmuluku on väärin", nodeCount, testResults[1].getNodeCount());
+        assertEquals("Ensimmäisen tuloksen tiedostopolku on väärin", filePath, testResults[0].getFilePath());
+        assertEquals("Toisen tuloksen tiedostopolku on väärin", filePath, testResults[1].getFilePath());
     }
     
 }
--- src/test/java/tiralabra/ui/MessengerTest.java
+++ src/test/java/tiralabra/ui/MessengerTest.java
@@ -1,8 +1,14 @@
 package tiralabra.ui;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
 import tiralabra.datastructures.NamedArrayList;
 import tiralabra.datastructures.PathStack;
 import tiralabra.domain.PathSearchResultSet;
@@ -11,12 +17,34 @@ import tiralabra.enums.AlgorithmAlternative;
 
 public class MessengerTest {
     
-    public MessengerTest() {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private PrintStream oldErr;
+    
+    private PathSearchResultSet resultSet;
+    private PathStack path;
+    private PlaceNode startNode;
+    private PlaceNode endNode;
+    private long runTimeNanoSecs;
+    private AlgorithmAlternative algorithm;
+    private String filePath = "c://joku/tiedosto.data";
+    
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+        oldErr = System.err;
+        System.setErr(new PrintStream(errContent));
+        
+        resultSet = Mockito.mock(PathSearchResultSet.class);
+        
     }
     
-    @BeforeClass
-    public static void setUpClass() {
+    @After
+    public void tearDown() {
+        System.setOut(null);
+        System.setErr(oldErr);
     }
+
 
     @Test
     public void testShowResults() {
@@ -35,37 +63,54 @@ public class MessengerTest {
 
     @Test
     public void testPrintResultMetaData() {
-        
+        String expected = "Tack och välkommen åter!\n";
+        Messenger.printGoodbye();
+        assertEquals("Tulostettu hyvästely on väärin", expected, outContent.toString());
     }
 
     @Test
     public void testPrintParamError() {
-        
+        String expected = "Anna polun etsintäkäsky muodossa [tiedostopolku] [lähtöpaikka] [maalipaikka]!\n";
+        expected += "Jos haluat käyttää edellistä datatiedostoa, voit korvata tiedostopolun +-merkillä.\n";
+        expected += "Voit tulostaa kaikki tähänastiset tulokset '=':lla ja lopettaa 'q':lla.\n";
+        Messenger.printParamError();
+        assertEquals(expected, outContent.toString());
     }
 
     @Test
     public void testPrintMessage() {
-        
+        String expected = "Joku odotettu viesti.";
+        Messenger.printMessage(expected);
+        assertEquals("Tulostettu viesti on väärin", expected  + "\n", outContent.toString());
     }
 
     @Test
     public void testPrintPrompt() {
-        
+        String expected = "Anna seuraava komento!\nOhje: [tiedostopolku] [lähtöpaikka] [maalipaikka] hakee polun, q lopettaa, = näyttää kaikki tulokset\n> ";
+        Messenger.printPrompt();
+        assertEquals("Tulostettu ohje/kehote on väärin", expected, outContent.toString());
     }
 
     @Test
     public void testPrintSeparator() {
-        
+        String expected = "----------------------------------------------------------------------------\n";
+        Messenger.printSeparator();
+        assertEquals("Tulostettu erotin on väärin", expected, outContent.toString());
     }
 
     @Test
     public void testPrintFileError() {
-        
+        String message = "Tiedostoa ei voitu avata.";        
+        String expected = "Datatiedoston c://joku/tiedosto.data luku ei onnistunut:\nTiedostoa ei voitu avata.\n";
+        Messenger.printFileError(message, filePath);
+        assertEquals("Tulostettu tiedostovirheilmoitus on väärin", expected, outContent.toString());
     }
 
     @Test
     public void testPrintGoodbye() {
-        
+        String expected = "Tack och välkommen åter!\n";
+        Messenger.printGoodbye();
+        assertEquals("Tulostettu hyvästely on väärin", expected, outContent.toString());
     }
     
 }
